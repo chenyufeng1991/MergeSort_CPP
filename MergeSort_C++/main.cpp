21a6f0b4099668b46ed44860be96178bd3d4cb33
陈宇峰 <yufengcode@gmail.com>
2016-09-24 23:36:38 +0800

    init

--- MergeSort_C++/main.cpp
+++ MergeSort_C++/main.cpp
@@ -7,9 +7,78 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+/**
+ *  归并排序时间复杂度O(nlogn),空间复杂度O(1),是一种稳定的排序
+ */
+
+void MergeSort(vector<int> &vec,int left, int right, vector<int> &temp);
+void MergeArray(vector<int> &vec, int left, int mid, int right, vector<int> &temp);
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {4,2,7,1,0,9};
+    vector<int>vectorArray(arr, arr + sizeof(arr) / sizeof(int));
+    vector<int>temp(6);
+    MergeSort(vectorArray, 0, 5, temp);
+
+    vector<int>::iterator vectorIte;
+    for (vectorIte = vectorArray.begin(); vectorIte != vectorArray.end(); vectorIte++)
+    {
+        cout << *vectorIte << " ";
+    }
+
     return 0;
 }
+
+void MergeArray(vector<int> &vec, int left, int mid, int right, vector<int> &temp)
+{
+    int i = left,j = mid;
+    int m = mid + 1,n = right;
+    int k = 0;
+
+    while (i <= j && m <= n)
+    {
+        if (vec[i] <= vec[m])
+        {
+            temp[k++] = vec[i++];
+        }
+        else
+        {
+            temp[k++] = vec[m++];
+        }
+    }
+
+    while (i <= j)
+    {
+        temp[k++] = vec[i++];
+    }
+
+    while (m <= n)
+    {
+        temp[k++] = vec[m++];
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        vec[left + i] = temp[i];
+    }
+}
+
+void MergeSort(vector<int> &vec,int left, int right, vector<int> &temp)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        MergeSort(vec, left, mid, temp);
+        MergeSort(vec, mid + 1, right, temp);
+        MergeArray(vec, left, mid, right, temp);
+    }
+}
+
+
+
+
